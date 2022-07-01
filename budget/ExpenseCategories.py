71ab5bca63f68db7a0f3edfbf50d46f72ee7c357
Juby132 <jubyagwu@gmail.com>
2022-06-30 22:56:11 -0400

    'module5'

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -10,13 +10,19 @@ def main():
     divided_set_comp = expenses.categorize_set_comprehension()
     if divided_for_loop != divided_set_comp:
         print('Sets are NOT equal by == test')
+
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
+
     print(
         timeit.timeit(
             stmt="expenses.categorize_for_loop()",
-            setup="\nfrom . import Expense\nexpenses = Expense.Expenses()\nexpenses.read_expenses('data/spending_data.csv')",
+            setup="""
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+            """,
             number=100000,
             globals=globals()
         )
@@ -24,7 +30,11 @@ def main():
     print(
         timeit.timeit(
             stmt="expenses.categorize_set_comprehension()",
-            setup="\nfrom . import Expense\nexpenses = Expense.Expenses()\nexpenses.read_expenses('data/spending_data.csv')",
+            setup="""
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+""",
             number=100000,
             globals=globals()
         )
